80316dac1965f5e01348b7c814bff185eceb7d00
Mildred <mbrooklyn83@gmail.com>
2021-09-15 20:59:06 -0400

    script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const button = document.getElementById('button');
+
+const colors = ['teal','orange','yellow','green','blue','rebeccapurple','violet'];
+
+function change() {      
+  document.body.style.background = colors[Math.floor(7*Math.random())];
+}
+
+button.addEventListener('click', change);
